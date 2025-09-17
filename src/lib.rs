1fdd7271d4b522a0b51a95eb27d259e13674e652
The-Infinitys <the.infinity.s.infinity@gmail.com>
2025-09-17 09:01:43 +0900

    reset

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,4 @@
-use crate::components::main_widget::{
-    editor::Editor, settings_editor::SettingsEditor, welcome_widget::WelcomeWidget,
-};
-
+pub mod utils;
 pub mod app;
-pub mod components;
-pub mod event_handler;
-pub mod settings;
-pub mod theme;
-pub mod ui;
-
-#[derive(PartialEq, Eq, Clone, Copy, Debug)]
-pub enum ActiveTarget {
-    Editor,
-    Panel,
-    PrimarySideBar,
-    SecondarySideBar,
-}
-
-pub enum MainWidgetContent {
-    Editor(Editor),
-    SettingsEditor(SettingsEditor),
-    Welcome(WelcomeWidget),
-}
-
-pub use components::popup::Popup;
-pub use components::popup::PopupResult;
+mod modules;
+pub use modules::*;
