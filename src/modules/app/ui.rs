45e24364b76133a1660fab47bc0235bcb65108e2
The-Infinitys <the.infinity.s.infinity@gmail.com>
2025-09-21 20:13:24 +0900

    fix: configure files

--- src/modules/app/ui.rs
+++ src/modules/app/ui.rs
@@ -16,24 +16,6 @@ pub struct ExtensionWidget {
 	pub binary_path: String,
 	// 必要に応じて追加
 }
-pub mod bottom_bar;
-pub mod command_palette;
-pub mod layout;
-pub mod left_panel;
-pub mod main_panel;
-pub mod notifications;
-pub mod right_panel;
-pub mod sub_panel;
-pub mod tab_bar;
-pub mod top_bar;
 
-pub use bottom_bar::draw_bottom_bar;
-pub use command_palette::draw_command_palette;
-pub use layout::{split_center_box, split_middle_box};
-pub use left_panel::draw_left_panel;
-pub use main_panel::draw_main_panel;
-pub use notifications::draw_notifications;
-pub use right_panel::draw_right_panel;
-pub use sub_panel::draw_sub_panel;
-pub use tab_bar::{draw_horizontal_tab_bar, draw_vertical_tab_bar};
-pub use top_bar::draw_top_bar;
+pub mod bar;
+pub mod panel;
---
+++ src/modules/app/ui/panel.rs
@@ -0,0 +1,4 @@
+pub mod left_panel;
+pub mod main_panel;
+pub mod right_panel;
+pub mod sub_panel;
